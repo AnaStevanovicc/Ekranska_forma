e0bc2dff92c8d8e302c3033742af8450182d2e05
USER <USER@DESKTOP-UBL32UH>
2021-12-19 22:27:15 +0100

    Add project files.

---
+++ Ekranska_forma/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Ekranska_forma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNaziv = new System.Windows.Forms.TextBox();
+            this.txtSprat = new System.Windows.Forms.TextBox();
+            this.txtBrUcenika = new System.Windows.Forms.TextBox();
+            this.txtBrRacunara = new System.Windows.Forms.TextBox();
+            this.txtProjektor = new System.Windows.Forms.TextBox();
+            this.insertbutton = new System.Windows.Forms.Button();
+            this.updatebutton = new System.Windows.Forms.Button();
+            this.deletebutton = new System.Windows.Forms.Button();
+            this.previousbutton = new System.Windows.Forms.Button();
+            this.nextbutton = new System.Windows.Forms.Button();
+            this.firstbutton = new System.Windows.Forms.Button();
+            this.lastbutton = new System.Windows.Forms.Button();
+            this.Ocistiti = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(108, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(108, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Naziv";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(108, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sprat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(108, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Broj ucenika";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(112, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Broj racunara";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(108, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Projektor";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(234, 72);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 6;
+            // 
+            // txtNaziv
+            // 
+            this.txtNaziv.Location = new System.Drawing.Point(234, 107);
+            this.txtNaziv.Name = "txtNaziv";
+            this.txtNaziv.Size = new System.Drawing.Size(100, 20);
+            this.txtNaziv.TabIndex = 7;
+            // 
+            // txtSprat
+            // 
+            this.txtSprat.Location = new System.Drawing.Point(234, 141);
+            this.txtSprat.Name = "txtSprat";
+            this.txtSprat.Size = new System.Drawing.Size(100, 20);
+            this.txtSprat.TabIndex = 8;
+            // 
+            // txtBrUcenika
+            // 
+            this.txtBrUcenika.Location = new System.Drawing.Point(234, 175);
+            this.txtBrUcenika.Name = "txtBrUcenika";
+            this.txtBrUcenika.Size = new System.Drawing.Size(100, 20);
+            this.txtBrUcenika.TabIndex = 9;
+            // 
+            // txtBrRacunara
+            // 
+            this.txtBrRacunara.Location = new System.Drawing.Point(234, 213);
+            this.txtBrRacunara.Name = "txtBrRacunara";
+            this.txtBrRacunara.Size = new System.Drawing.Size(100, 20);
+            this.txtBrRacunara.TabIndex = 10;
+            // 
+            // txtProjektor
+            // 
+            this.txtProjektor.Location = new System.Drawing.Point(234, 251);
+            this.txtProjektor.Name = "txtProjektor";
+            this.txtProjektor.Size = new System.Drawing.Size(100, 20);
+            this.txtProjektor.TabIndex = 11;
+            // 
+            // insertbutton
+            // 
+            this.insertbutton.Location = new System.Drawing.Point(112, 304);
+            this.insertbutton.Name = "insertbutton";
+            this.insertbutton.Size = new System.Drawing.Size(83, 29);
+            this.insertbutton.TabIndex = 12;
+            this.insertbutton.Text = "INSERT";
+            this.insertbutton.UseVisualStyleBackColor = true;
+            this.insertbutton.Click += new System.EventHandler(this.insertbutton_Click);
+            // 
+            // updatebutton
+            // 
+            this.updatebutton.Location = new System.Drawing.Point(234, 304);
+            this.updatebutton.Name = "updatebutton";
+            this.updatebutton.Size = new System.Drawing.Size(82, 29);
+            this.updatebutton.TabIndex = 13;
+            this.updatebutton.Text = "UPDATE";
+            this.updatebutton.UseVisualStyleBackColor = true;
+            this.updatebutton.Click += new System.EventHandler(this.updatebutton_Click);
+            // 
+            // deletebutton
+            // 
+            this.deletebutton.Location = new System.Drawing.Point(350, 304);
+            this.deletebutton.Name = "deletebutton";
+            this.deletebutton.Size = new System.Drawing.Size(78, 29);
+            this.deletebutton.TabIndex = 14;
+            this.deletebutton.Text = "DELETE";
+            this.deletebutton.UseVisualStyleBackColor = true;
+            this.deletebutton.Click += new System.EventHandler(this.deletebutton_Click);
+            // 
+            // previousbutton
+            // 
+            this.previousbutton.Location = new System.Drawing.Point(165, 348);
+            this.previousbutton.Name = "previousbutton";
+            this.previousbutton.Size = new System.Drawing.Size(83, 29);
+            this.previousbutton.TabIndex = 15;
+            this.previousbutton.Text = "<";
+            this.previousbutton.UseVisualStyleBackColor = true;
+            this.previousbutton.Click += new System.EventHandler(this.previousbutton_Click);
+            // 
+            // nextbutton
+            // 
+            this.nextbutton.Location = new System.Drawing.Point(292, 348);
+            this.nextbutton.Name = "nextbutton";
+            this.nextbutton.Size = new System.Drawing.Size(79, 29);
+            this.nextbutton.TabIndex = 16;
+            this.nextbutton.Text = ">";
+            this.nextbutton.UseVisualStyleBackColor = true;
+            this.nextbutton.Click += new System.EventHandler(this.nextbutton_Click);
+            // 
+            // firstbutton
+            // 
+            this.firstbutton.Location = new System.Drawing.Point(165, 395);
+            this.firstbutton.Name = "firstbutton";
+            this.firstbutton.Size = new System.Drawing.Size(83, 29);
+            this.firstbutton.TabIndex = 17;
+            this.firstbutton.Text = "<<";
+            this.firstbutton.UseVisualStyleBackColor = true;
+            this.firstbutton.Click += new System.EventHandler(this.firstbutton_Click);
+            // 
+            // lastbutton
+            // 
+            this.lastbutton.Location = new System.Drawing.Point(292, 395);
+            this.lastbutton.Name = "lastbutton";
+            this.lastbutton.Size = new System.Drawing.Size(79, 29);
+            this.lastbutton.TabIndex = 18;
+            this.lastbutton.Text = ">>";
+            this.lastbutton.UseVisualStyleBackColor = true;
+            this.lastbutton.Click += new System.EventHandler(this.lastbutton_Click);
+            // 
+            // Ocistiti
+            // 
+            this.Ocistiti.Location = new System.Drawing.Point(234, 436);
+            this.Ocistiti.Name = "Ocistiti";
+            this.Ocistiti.Size = new System.Drawing.Size(75, 54);
+            this.Ocistiti.TabIndex = 19;
+            this.Ocistiti.Text = "Ocistiti unete podatke";
+            this.Ocistiti.UseVisualStyleBackColor = true;
+            this.Ocistiti.Click += new System.EventHandler(this.Ocistiti_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 502);
+            this.Controls.Add(this.Ocistiti);
+            this.Controls.Add(this.lastbutton);
+            this.Controls.Add(this.firstbutton);
+            this.Controls.Add(this.nextbutton);
+            this.Controls.Add(this.previousbutton);
+            this.Controls.Add(this.deletebutton);
+            this.Controls.Add(this.updatebutton);
+            this.Controls.Add(this.insertbutton);
+            this.Controls.Add(this.txtProjektor);
+            this.Controls.Add(this.txtBrRacunara);
+            this.Controls.Add(this.txtBrUcenika);
+            this.Controls.Add(this.txtSprat);
+            this.Controls.Add(this.txtNaziv);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "  ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNaziv;
+        private System.Windows.Forms.TextBox txtSprat;
+        private System.Windows.Forms.TextBox txtBrUcenika;
+        private System.Windows.Forms.TextBox txtBrRacunara;
+        private System.Windows.Forms.TextBox txtProjektor;
+        private System.Windows.Forms.Button insertbutton;
+        private System.Windows.Forms.Button updatebutton;
+        private System.Windows.Forms.Button deletebutton;
+        private System.Windows.Forms.Button previousbutton;
+        private System.Windows.Forms.Button nextbutton;
+        private System.Windows.Forms.Button firstbutton;
+        private System.Windows.Forms.Button lastbutton;
+        private System.Windows.Forms.Button Ocistiti;
+    }
+}
+
---
+++ Ekranska_forma/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Ekranska_forma
+{
+    public partial class Form1 : Form
+    {
+        string cs = "Data source = DESKTOP-UBL32UH; Initial catalog = Skola; Integrated security = true";
+        DataTable podaci = new DataTable();
+        SqlConnection veza;
+        SqlDataAdapter adapter;
+        string naziv, sprat, br_ucenika, br_racunara, projektor;
+
+        private void nextbutton_Click(object sender, EventArgs e)
+        {
+            if (gde + 1 <= podaci.Rows.Count)
+            {
+                gde++;
+                osvezi();
+            }
+        }
+
+        private void previousbutton_Click(object sender, EventArgs e)
+        {
+            if (gde - 1 >= 0)
+            {
+                gde--;
+                osvezi();
+            }
+        }
+
+        private void firstbutton_Click(object sender, EventArgs e)
+        {
+            gde = 0;
+            osvezi();
+        }
+
+        private void lastbutton_Click(object sender, EventArgs e)
+        {
+            gde = podaci.Rows.Count - 1;
+            osvezi();
+        }
+
+        private void deletebutton_Click(object sender, EventArgs e)
+        {
+            veza = new SqlConnection(cs);
+            SqlCommand naredba = new SqlCommand(String.Format($"DELETE from Ucionica where id= {txtID.Text}"), veza);
+            veza.Open();
+            naredba.ExecuteNonQuery();
+            veza.Close();
+            podaci.Clear();
+            adapter = new SqlDataAdapter("SELECT * from Ucionica", veza);
+            adapter.Fill(podaci);
+            gde = 0;
+            osvezi();
+
+        }
+
+        private void insertbutton_Click(object sender, EventArgs e)
+        {
+            veza = new SqlConnection(cs);
+            naziv = txtNaziv.Text;
+            sprat = txtSprat.Text;
+            br_ucenika = txtBrUcenika.Text;
+            br_racunara = txtBrRacunara.Text;
+            projektor = txtProjektor.Text;
+
+            veza.Open();
+            SqlCommand naredba = new SqlCommand($"insert into Ucionica values('{naziv}', '{sprat}', '{br_ucenika}', '{br_racunara}', '{projektor}')", veza);
+            naredba.ExecuteNonQuery();
+            veza.Close();
+            podaci.Clear();
+            adapter = new SqlDataAdapter("SELECT * from Ucionica", veza);
+            adapter.Fill(podaci);
+            gde = podaci.Rows.Count - 1;
+            osvezi();
+        }
+
+        private void updatebutton_Click(object sender, EventArgs e)
+        {
+            veza = new SqlConnection(cs);
+            naziv = txtNaziv.Text;
+            sprat = txtSprat.Text;
+            br_ucenika = txtBrUcenika.Text;
+            br_racunara = txtBrRacunara.Text;
+            projektor = txtProjektor.Text;
+
+            if (naziv == "" && sprat == "" && br_ucenika == "" && br_racunara == "" && projektor == "")
+            {
+                MessageBox.Show("Neophodno je uneti podatak.");
+            }
+
+            veza.Open();
+            SqlCommand naredba = new SqlCommand($"UPDATE Ucionica SET naziv = '{naziv}', sprat = '{sprat}', br_ucenika = '{br_ucenika}', br_racunara = '{br_racunara}', projektor = '{projektor}' WHERE id = {txtID.Text}", veza);
+            naredba.ExecuteNonQuery();
+            veza.Close();
+            podaci.Clear();
+            adapter = new SqlDataAdapter("SELECT * from Ucionica", veza);
+            adapter.Fill(podaci);
+            osvezi();
+        }
+
+        private void Ocistiti_Click(object sender, EventArgs e)
+        {
+            txtNaziv.Text = "";
+            txtSprat.Text = "";
+            txtBrUcenika.Text = "";
+            txtBrRacunara.Text = "";
+            txtProjektor.Text = "";
+        }
+
+        int gde = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            veza = new SqlConnection(cs);
+            adapter = new SqlDataAdapter("SELECT * from Ucionica", veza);
+            adapter.Fill(podaci);
+            txtID.Enabled = false;
+            osvezi();
+            
+        }
+
+        public void osvezi()
+        {
+            if (podaci.Rows.Count == 0)
+            {
+                updatebutton.Enabled = false;
+                deletebutton.Enabled = false;
+                firstbutton.Enabled = false;
+                lastbutton.Enabled = false;
+                previousbutton.Enabled = false;
+                nextbutton.Enabled = false;
+
+                txtID.Text = "";
+                txtNaziv.Text = "";
+                txtSprat.Text = "";
+                txtBrUcenika.Text = "";
+                txtBrRacunara.Text = "";
+                txtProjektor.Text = "";
+            }
+            else 
+            {
+                txtID.Text = podaci.Rows[gde]["id"].ToString();
+                txtNaziv.Text = podaci.Rows[gde]["naziv"].ToString();
+                txtSprat.Text = podaci.Rows[gde]["sprat"].ToString();
+                txtBrUcenika.Text = podaci.Rows[gde]["br_ucenika"].ToString();
+                txtBrRacunara.Text = podaci.Rows[gde]["br_racunara"].ToString();
+                txtProjektor.Text = podaci.Rows[gde]["projektor"].ToString();
+
+                firstbutton.Enabled = (gde != 0);
+                lastbutton.Enabled = (gde != podaci.Rows.Count - 1);
+                previousbutton.Enabled = (gde != 0);
+                nextbutton.Enabled = (gde != podaci.Rows.Count -1 );
+            }
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
